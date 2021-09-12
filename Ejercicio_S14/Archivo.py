8f15fe9d082192d10279a0f5a62a80fbc111bf2e
MARILEONV <85856591+MARILEONV@users.noreply.github.com>
2021-09-12 11:36:07 -0500

    Add files via upload

---
+++ Ejercicio_S14/Archivo.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: maril
+"""
+
+archivo,f= "datos.txt", " "
+docentes= [{"nombre":"Daniel","edad":50, "fac":"Ingenieria"},
+           {"nombre":"María","edad":19, "fac":"Economía"},
+           {"nombre":"Aylin","edad":21, "fac":"Ciencas"} ]
+
+# Escribir archivo: w-a: write-writeline
+with open(archivo,"w") as writter:
+    for i in range(len(docentes)):
+        linea= " "
+        for clave, valor in docentes[i].items():
+            if clave=="fac":
+                linea= linea+ (str(valor) if type(valor)==int else valor) + "\n"
+            else:
+                linea= linea+ (str(valor) if type(valor)==int else valor) + ";"
+        writter.writelines(linea)
+
+# Leer archivo: r: read - readline - readlines - in
+try:
+    f= open(archivo,"r")
+    for linea in f:
+        print(linea[:-1])
+except Exception as e:
+    print("Error de archivo: "+str(e))
+finally:
+   f.close()
